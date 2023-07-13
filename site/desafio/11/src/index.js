3a3855fef6861753518e13d1e30e00a728f4137e
MateusGoulart20 <mateusgoulart20@hotmail.com>
2023-07-12 20:38:21 -0400

    a

--- site/desafio/11/src/index.js
+++ site/desafio/11/src/index.js
@@ -1,6 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -11,3 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
