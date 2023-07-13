a8bb60397239d8b8641844f0f5fb34af119ca12e
MateusGoulart20 <mateusgoulart20@hotmail.com>
2023-07-12 21:51:09 -0400

    wuupu

--- site/desafio/11/src/App.jsx
+++ site/desafio/11/src/App.jsx
@@ -1,186 +1,107 @@
-import './App.css';
-
-export default function App() {
-  return (
-    <div>
-      Boa noite povo bonito
-      
-    </div>
-  );
-}
-/*
 // npm install react-bootstrap bootstrap react-hook-form
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useForm } from 'react-hook-form';
-
 import { Input } from './components/Input';
+
+/*
+- “Nome Completo“: obrigatório
+- “E-mail“: obrigatório
+- “Data de nascimento“: obrigatório, deve ser maior de idade
+- “Aceito receber novidades por e-mail“: um checkbox que o padrão já vem
+selecionado
+- Botão “Cadastrar“: ao clicar apenas mostre os dados no console para teste
+*/
 
 // React Bootstrap + React Hook Form
 export default function App() {
-    const { handleSubmit, register, formState: { errors } } = useForm();
+  const { handleSubmit, register, formState: { errors } } = useForm();
 
-    const onSubmit = async (data) => {
-        try {
-            const result = await fetch('http://localhost:8080/register', {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
-            const token = await result.json();
-            console.log(token);
-        } catch (error) {
-            console.error(error);
+  const onSubmit = async (data) => {
+    console.log(data)
+    /*try {
+      const result = await fetch('http://localhost:8080/register', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
         }
-    }
+      });
+      const token = await result.json();
+      console.log(token);
+    } catch (error) {
+      console.error(error);
+    }*/
+  }
+  return (
 
-    return (
-        <Form
-            noValidate
-            validated={!!errors}
-            onSubmit={handleSubmit(onSubmit)}
-        >
-            <Row>
-                <Col>
-                    <Input
-                        label="Nome"
-                        type="text"
-                        placeholder="Insira seu Nome"
-                        error={errors.name}
-                        required={true}
-                        name="name"
-                        validations={register('name', {
-                            required: {
-                                value: true,
-                                message: 'Nome é obrigatório'
-                            },
-                        })}
-                    />
-                </Col>
-                <Col>
-                    <Input
-                        label="Senha"
-                        type="password"
-                        placeholder="Insira sua senha"
-                        error={errors.password}
-                        required={true}
-                        name="password"
-                        validations={register('password', {
-                            required: {
-                                value: true,
-                                message: 'Senha é obrigatório'
-                            }
-                        })}
-                    />
-                </Col>
-            </Row>
-            <Button type="submit">Enviar</Button>
-        </Form>
-    );
+      <Form
+        noValidate
+        validated={!!errors}
+        onSubmit={handleSubmit(onSubmit)}
+        className='w-50 bg-red'
+
+      >
+        <Col>
+          <Row>
+            <Input
+              label="Nome Completo"
+              type="text"
+              placeholder="Insira seu Nome"
+              error={errors.name}
+              required={true}
+              name="name"
+              validations={register('name', {
+                required: {
+                  value: true,
+                  message: 'Nome é obrigatório'
+                },
+              })}
+            />
+          </Row>
+          <Row>
+            <Input
+              label="Senha"
+              type="password"
+              placeholder="Insira sua senha"
+              error={errors.password}
+              required={true}
+              name="password"
+              validations={register('password', {
+                required: {
+                  value: true,
+                  message: 'Senha é obrigatório'
+                }
+              })}
+          />
+        </Row>
+        <Row>
+          <Input
+            label="Data de nascimento"
+            type="date"
+            placeholder="Insira sua senha"
+            error={errors.password}
+            required={true}
+            name="birthday"
+            validations={register('birthday', {
+              required: {
+                value: true,
+                message: 'Nascimento é obrigatório'
+              }
+            })}
+          />
+        </Row>
+        <Row>
+
+          <Form.Check // prettier-ignore
+            id='news'
+            label='Aceito receber novidades por e-mail'
+            type='checkbox'
+            checked
+            {...register('news')}
+          />
+        </Row>
+      </Col>
+      <Button type="submit">Cadastrar</Button>
+    </Form>
+  );
 }
-
-*/
-
-/*
-import { Form, Button, Col, Row, Container } from 'react-bootstrap';
-import { useForm } from 'react-hook-form';
-
-export default function App() {
-    const { handleSubmit, register, formState: { errors } } = useForm();
-
-    const onSubmit = (data) => {
-        console.log(data);
-    }
-
-    return (
-        <div className='col-md mx-auto'>
-            <Container fluid className='p-5'>
-                <Form noValidate validated={!!errors} onSubmit={handleSubmit(onSubmit)}>
-                    <Row>
-                        <Form.Group as={Col} sm className='mb-3'>
-                            <Form.Label>Nome Completo</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="name"
-                                id="name"
-                                required
-                                placeholder="Insira seu nome completo"
-                                {...register('name', {
-                                    required: 'Nome Completo é obrigatório'
-                                })}
-                            />
-                            {errors.name && <Form.Control.Feedback type="invalid">{errors.fullName.message}</Form.Control.Feedback>}
-                        </Form.Group>
-                        <Form.Group as={Col} sm className='mb-3'>
-                            <Form.Label>CEP</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="cep"
-                                id="cep"
-                                isInvalid={errors.cep}
-                                placeholder="Insira seu CEP"
-                                {...register('cep', {
-                                    pattern: {
-                                        value: /\d{5}-\d{3}/,
-                                        message: 'CEP inválido'
-                                    },
-                                })}
-                            />
-                            {errors.cep && <Form.Control.Feedback type="invalid">{errors.cep.message}</Form.Control.Feedback>}
-                        </Form.Group>
-                    </Row>
-                    <Row>
-                        <Form.Group as={Col} sm className='mb-3'>
-                            <Form.Label>E-mail</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="email"
-                                id="email"
-                                required
-                                isInvalid={errors.email}
-                                placeholder="Insira seu e-mail"
-                                {...register('email', {
-                                    required: 'E-mail é obrigatório',
-                                    pattern: {
-                                        value: /^\S+@\S+$/i,
-                                        message: 'E-mail inválido'
-                                    }
-                                })}
-                            />
-                            {errors.email && <Form.Control.Feedback type="invalid">{errors.email.message}</Form.Control.Feedback>}
-                        </Form.Group>
-                        <Form.Group as={Col} sm className='mb-3'>
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control
-                                type='password'
-                                name="password"
-                                id="password"
-                                required
-                                isInvalid={errors.password}
-                                placeholder="Insira sua senha"
-                                {...register('password', {
-                                    required: {
-                                        value: true,
-                                        message: 'Senha é obrigatória'
-                                    },
-                                    minLength: {
-                                        value: 5,
-                                        message: 'A senha deve ter no mínimo 5 caracteres'
-                                    }
-                                })}
-                            />
-                            {errors.password && <Form.Control.Feedback type="invalid">{errors.password.message}</Form.Control.Feedback>}
-                        </Form.Group>
-                    </Row>
-                    <Form.Group as={Col} className="mb-5">
-                        <Form.Check type="checkbox" label="Condordo com os termos de serviço." />
-                    </Form.Group>
-                    <Button type="submit">Enviar</Button>
-                </Form>
-            </Container>
-        </div>
-    );
-}
-
-*/
