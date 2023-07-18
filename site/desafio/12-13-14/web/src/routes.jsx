da2180229832688ecfb492867fccfd6223ad68dc
MateusGoulart20 <mateusgoulart20@hotmail.com>
2023-07-17 20:34:08 -0400

    update

---
+++ site/desafio/12-13-14/web/src/routes.jsx
@@ -0,0 +1,42 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
+import { Foods } from "./pages/Foods";
+import { FoodsRead } from "./pages/FoodsRead";
+
+import { isAuthenticated } from './utils/is-authenticated';
+
+export function PrivateRoute({ children }) {
+    if (!isAuthenticated()) {
+        return <Navigate to="/" replace />
+    }
+    return children;
+}
+
+export function Navigations() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route index path="/" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route
+                    path="/foods"
+                    element={(
+                        <PrivateRoute>
+                            <Foods />
+                        </PrivateRoute>
+                    )}
+                />
+                <Route
+                    path="/foods/view"
+                    element={(
+                        <PrivateRoute>
+                            <FoodsRead />
+                        </PrivateRoute>
+                    )}
+                />
+            </Routes>
+        </BrowserRouter>
+    )
+}
